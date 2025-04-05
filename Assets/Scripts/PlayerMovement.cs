5e3b1dcbd587ad3291503e629185ada9d04f5d2d
GFiatt <100244630+GFiatt@users.noreply.github.com>
2025-04-04 18:50:32 -0600

    Avance con animación de pájaro

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,64 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEditor;
+
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movement;
-    private Rigidbody2D rb;
+    [Header("References")]
+    [SerializeField] private Transform birdRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
 
-    private void Awake()
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 3f;
+    [SerializeField] private float rotationSpeed = 200f;
+
+    private Transform target;
+
+    private void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        if (target != null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+
     }
-    private void OnMovement(InputValue value)
+
+    private void FindTarget()
     {
-        movement = value.Get<Vector2>();
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2) transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
     }
-    private void FixedUpdate()
+
+    private bool CheckTargetIsInRange()
     {
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
     }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2((target.position.y - transform.position.y), (target.position.x - transform.position.x)) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        birdRotationPoint.rotation = Quaternion.RotateTowards(birdRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+    }
+
+
 }
